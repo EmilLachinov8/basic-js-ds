eb05ea7c8f91bebed28f9562c01f3b3c70ed22fe
EmilLachinov8 <lachinov_em@mail.ru>
2023-04-18 02:43:40 +0300

    init: make tasks

--- src/queue.js
+++ src/queue.js
@@ -5,7 +5,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
- * @example
+ * example
  * const queue = new Queue();
  *
  * queue.enqueue(1); // adds the element to the queue
@@ -14,20 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const newNode = { value: value, next: null };
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
+      this.tail = newNode;
+    }
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return undefined;
+    }
+    const dequeuedValue = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return dequeuedValue;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -6,11 +6,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
  *
- * @param {List} l
- * @param {Number} k
- * @return {List}
+ * param {List} l
+ * param {Number} k
+ * return {List}
  *
- * @example
+ * example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) {
+    return null;
+  }
+  
+  while (l && l.value === k) {
+    l = l.next;
+  }
+  
+  if (!l) {
+    return null;
+  }
+  
+  let previous = l;
+  let current = l.next;
+  
+  while (current) {
+    if (current.value === k) {
+      previous.next = current.next;
+    } else {
+      previous = current;
+    }
+    
+    current = current.next;
+  }
+  
+  return l;
 }
 
 module.exports = {
